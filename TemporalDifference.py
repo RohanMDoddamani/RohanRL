577c9e42bb771ccaa691c48e10509689350c498e
rohan.doddamani <rohan136.md@gmail.com>
2025-07-24 16:16:25 +0530

    td(0) update

--- TemporalDifference.py
+++ TemporalDifference.py
@@ -1,32 +1,31 @@
-import gym
+import gymnasium as gym
 import numpy as np
 from collections import defaultdict
 
 # Environment
-env = gym.make("FrozenLake-v1", is_slippery=False)  # deterministic for simplicity
+env = gym.make("FrozenLake-v1", is_slippery=True)  
+# env = gym.make("FrozenLake-v1", is_slippery=False)  deterministic for simplicity
 
 # Parameters
 alpha = 0.1         # learning rate
-gamma = 0.99        # discount factor
+gamma = 0.7        # discount factor
 num_episodes = 5000
 
-# Initialize value function
-V = defaultdict(float)
 
-# Define a simple policy: move right (2) or down (1) with equal probability
+V = {i:0 for i in range(env.observation_space.n)}
+
 def policy(state):
     return np.random.choice([1, 2])  # down or right
 
 # TD(0) algorithm
-for episode in range(num_episodes):
+for episode in range(100):
     state = env.reset()[0]  # Gym v0.26+ returns (obs, info)
     done = False
 
     while not done:
         action = policy(state)
         next_state, reward, done, truncated, info = env.step(action)
-        
-        # TD(0) update
+
         V[state] += alpha * (reward + gamma * V[next_state] - V[state])
         
         state = next_state
@@ -34,3 +33,6 @@ for episode in range(num_episodes):
 # Display learned value function
 for s in range(env.observation_space.n):
     print(f"V({s}) = {V[s]:.2f}")
+
+
+
