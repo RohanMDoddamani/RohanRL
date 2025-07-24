3763887b07e4e7bef5c12a18314f571f4b5c9203
rohan.doddamani <rohan136.md@gmail.com>
2025-07-24 16:53:33 +0530

    DickeyFiller updation

--- DickeyFiller.py
+++ DickeyFiller.py
@@ -9,11 +9,24 @@ n = 100
 white_noise = np.random.normal(0, 1, n)
 
 # Perform Augmented Dickey-Fuller (ADF) test
-adf_result = adfuller(white_noise)
 
-print(f"ADF Statistic: {adf_result[0]}")
-print(f"P-Value: {adf_result[1]}")
-if adf_result[1] < 0.05:
-    print("Stationary process (Reject Null Hypothesis)")
-else:
-    print("Non-Stationary process (Fail to Reject Null Hypothesis)")
+adf_result_st = adfuller(white_noise)
+
+# Add linear trend to make it non-stationary
+
+trend = 0.2 * np.arange(n)
+nonstationary = trend + white_noise
+
+adf_result_nst = adfuller(nonstationary)
+
+checkSt = [adf_result_st,adf_result_nst]
+
+for i in checkSt:
+    print(f"ADF Statistic: {i[0]}")
+    print(f"P-Value: {i[1]}")
+    if i[1] < 0.05:
+        print("Stationary process (Reject Null Hypothesis)")
+    else:
+        print("Non-Stationary process (Fail to Reject Null Hypothesis)")
+
+
